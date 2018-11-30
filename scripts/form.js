280845d777fe94b74f08304680d847c964d840b9
Mathias Plans <mathiasplans15@gmail.com>
2018-11-30 23:34:04 +0200

    Lisasin kontakt vormi

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,7 @@
 isikjamaksSubmit = () => {
   event.preventDefault();
   $("#isik-ja-maks").hide();
+  $("#kotakt-form").hide();
   $("#loading-please-wait").removeClass('hidden');
   return true;
 };
